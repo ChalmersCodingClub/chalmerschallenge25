4599dd0616d04d7570dbef4699b5d9834b70882c
Hugo Söderbergh <hugo.soderbergh007@gmail.com>
2025-01-20 23:20:13 +0100

    Make larger testcases and add sample to tidsfordriv

--- problems/tidsfordriv/data/generator.sh
+++ problems/tidsfordriv/data/generator.sh
@@ -7,8 +7,12 @@ use_solution hugo.cpp
 
 compile gen_random.py
 
+samplegroup
+sample 1
+
 group group01 100
+tc 1
 for i in {1..25}
 do
-tc g1-$i gen_random max_n=10
+tc g1-$i gen_random max_n=51
 done
