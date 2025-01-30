1f34c5431e4c896a11e1cd71bd2712472c6668c0
Hugo Söderbergh <hugo.soderbergh007@gmail.com>
2025-01-30 17:27:54 +0100

    Add more samples and add an explanation for one of the samples

--- problems/tidsfordriv/data/generator.sh
+++ problems/tidsfordriv/data/generator.sh
@@ -9,15 +9,19 @@ compile gen_random.py
 
 samplegroup
 sample 1
+sample 2
+sample 3
 
 group group01 20
 tc 1
+tc 2
 for i in {1..9}
 do
 tc g1-$i gen_random n=$i
 done
 
 group group02 80
+tc 3
 tc g2-1  gen_random n=32
 tc g2-2  gen_random n=28
 tc g2-3  gen_random n=31
