fbefbf1ceda1df4325e339540d40af8515ac4a30
Hugo Söderbergh <hugo.soderbergh007@gmail.com>
2025-01-30 22:20:38 +0100

    change ccclager testdata to only allow unique names

--- problems/ccclager/data/gen_weird.py
+++ problems/ccclager/data/gen_weird.py
@@ -14,23 +14,35 @@ def cmdlinearg(name, default=None):
 
 random.seed(int(cmdlinearg('seed', sys.argv[-1])))
 
+used_names = set()
 letters = "abcdefghijklmnopqrstuvwxyz"
 def gen_name():
-    x = random.randint(2, 9)
-    name = ''.join([random.choice(letters).upper()] + [random.choice(letters) for _ in range(x)])
-    p = random.randint(0, 3)
-    if p == 0:
-        name = "Joshua"
-    if p == 1:
-        name = "Gustav"
+    x = random.randint(3, 10)
+    name = ''.join([random.choice(letters).upper()] + [random.choice(letters) for _ in range(x-1)])
+    while name in used_names:
+        x = random.randint(3, 10)
+        name = ''.join([random.choice(letters).upper()] + [random.choice(letters) for _ in range(x-1)])
+    used_names.add(name)
     return name
 
 # room size: (spacing + 1) * (n + 1) - 1
 def woven_pattern(n, spacing):
+    names = []
+    if random.randint(0,1):
+        names.append("Joshua")
+        used_names.add("Joshua")
+
+    if random.randint(0,1) and len(names) < n:
+        names.append("Gustav")
+        used_names.add("Gustav")
+
+    if len(names) < n:
+        names.extend(gen_name() for _ in range(n - len(names)))
+
     m = n + 1
     print(n,m)
     for i in range(n):
-        print(gen_name(), (spacing + 1) * (i + 1) - 1)
+        print(names[i], (spacing + 1) * (i + 1) - 1)
     for i in range(m):
         print((spacing + 1) * i, (spacing + 1) * (i + 1) - 1 - random.randint(0, 1))
 
--- problems/ccclager/input_validators/validator/validator.cpp
+++ problems/ccclager/input_validators/validator/validator.cpp
@@ -1,6 +1,7 @@
 #include "validator.h"
 
 #include <bits/stdc++.h>
+#include <cctype>
 using namespace std;
 
 void run() {
@@ -11,13 +12,20 @@ void run() {
     int m = Int(0, Arg("maxm", 200000));
     Endl();
 
+    unordered_set<string> names;
     for (int i = 0; i < n; i++) {
         string name = _token();
+        if (names.find(name) != names.end()) {
+            die("Input may not contain duplicate names: \"" + name + "\"");
+        }
+        names.emplace(name);
         Space();
         int pos = Int(0, room);
-        assert(3 <= name.size() && name.size() <= 10);
-        assert(isupper(name[0]));
-        for (int j = 1; j < name.size(); j++) assert(islower(name[j]));
+        if (!(3 <= name.size() && name.size() <= 10)) die("Name: \"" + name + "\" has an invalid length (" + to_string(name.size()) + " characters");
+        if (!isupper(name[0])) die("Name: \"" + name + "\" does not have a capitalized first letter");
+        for (int j = 1; j < name.size(); j++) if (!islower(name[j])) {
+            die("Name: \"" + name + "\" has uppercase letters that is not the first letter");
+        }
         Endl();
     }
 
