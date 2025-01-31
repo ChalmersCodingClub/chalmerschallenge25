af9cce931bc30f741c28ddcfb472f3c6922aee61
Jens Kollberg <jensko@chalmers.se>
2025-01-31 22:49:57 +0100

    overnightoats python front list pop 100

---
+++ problems/overnightoats/submissions/accepted/jens_listfrontpop.py
@@ -0,0 +1,14 @@
+n = int(input())
+x = int(input())
+
+v = []
+for i in range(n):
+    s = input()
+    if s == 'ADD':
+        v.append(i)
+    if v and i - v[0] > x:
+        print("ono..")
+        exit(0)
+    elif s == 'EAT':
+        t = v.pop(0)
+print("yay!")
