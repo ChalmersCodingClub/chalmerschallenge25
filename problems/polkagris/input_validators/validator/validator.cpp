015b1356b117caf9fcd43037c248f24cf0154290
Gustav Kalander <gustav.kalander@hotmail.se>
2025-01-31 16:58:40 +0100

    polka fix

--- problems/polkagris/input_validators/validator/validator.cpp
+++ problems/polkagris/input_validators/validator/validator.cpp
@@ -5,29 +5,27 @@
 using namespace std;
 
 void run() {
-	int MAXN = Arg("maxn");
+    int MAXN = Arg("maxn");
 
-	int n = Int(1, MAXN);
-	Space();
+    int n = Int(1, MAXN);
+    Space();
 
-	int t = Int(1, MAXN);
-	Endl();
+    int t = Int(1, n);
+    Endl();
 
-        string initial, target;
-        cin >> initial;
-	Endl();
-        cin >> target;
-	Endl();
+    string initial = Line();
+    string target = Line();
 
-	Eof();
+    Eof();
 
-        assert(initial.size() == n);
-        assert(target.size() == t);
-
-        for (int i = 1; i < initial.size(); i++) {
-          assert(initial[i-1] != initial[i]);
-        }
-        for (int i = 1; i < target.size(); i++) {
-          assert(target[i-1] != target[i]);
-        }
+    assert(initial.size() == n);
+    assert(target.size() == t);
+    for(char c : initial) assert('A' <= c && c <= 'Z');
+    for(char c : target) assert('A' <= c && c <= 'Z');
+    for (int i = 1; i < initial.size(); i++) {
+        assert(initial[i-1] != initial[i]);
+    }
+    for (int i = 1; i < target.size(); i++) {
+        assert(target[i-1] != target[i]);
+    }
 }
